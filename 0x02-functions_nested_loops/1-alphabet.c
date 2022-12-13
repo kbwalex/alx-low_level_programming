5044ed4d1ee075bf21810e8fc515dd191ed4121e
kbwalex <kbwalex@gmail.com>
2022-12-13 10:49:18 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,20 @@
 
 /**
  * print_alphabet - print all alphabet in lowercase
+ *
+ * Return: 0
  */
 
 void print_alphabet(void)
 {
-	char c
+	char c = 'a';
 
-	for (c = 'a'; c <= 'z'; c++;)
+	while (c <= 'z')
 	{
 		_putchar(c);
+		c++;
 	}
+
 	_putchar('\n');
+
 }
