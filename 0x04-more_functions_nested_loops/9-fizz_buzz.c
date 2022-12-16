ee77e2d8f315e5910bb67422e27cded9b6f473f6
kbwalex <kbwalex@gmail.com>
2022-12-15 22:59:35 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,27 +9,18 @@ int main(void)
 {
 	int n;
 
-	n = 1;
-	printf("%d", n);
+	printf("1");
 	for (n = 2; n <= 100; n++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
-		{
-			printf("FizzBuzz");
-		}
-		else if (n % 3 == 0)
-		{
+		printf(" ");
+		if (n % 3 == 0)
 			printf("Fizz");
-		}
-		else if (n % 5 == 0)
-		{
+		if (n % 5 == 0)
 			printf("Buzz");
-		}
-		else
-		{
+		if (n % 3 != 0 && n % 5 != 0)
 			printf("%d", n);
-		}
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
+
