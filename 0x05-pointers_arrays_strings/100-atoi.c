ce42633f2aeb91da8c3d4d16c6ed72aed130e5a8
kbwalex <kbwalex@gmail.com>
2022-12-19 15:41:05 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,24 +11,24 @@
 
 int  _atoi(char *s)
 {
-	int sign, result, index;
+        int sign, result, index;
 
-	result = 0;
-	sign = 0;
+        result = 0;
+        sign = 0;
 
-	for (index = 0; s[index] != '\0'; index++)
-	{
-		if (s[index] == '-')
-		{
-			sign++;
-		}
-		if (s[index] > 47 && s[index] < 58)
-		{
-			while (s[index] > 47 && s[index] < 58)
+        for (index = 0; s[index] != '\0'; index++)
+        {
+                if (s[index] == '-')
+                {
+                        sign++;
+                }
+                if (s[index] > 47 && s[index] < 58)
+                {
+                        while (s[index] > 47 && s[index] < 58)
 
-				result = result * 10 - (s[index++] - 48);
-				break;
-		}
-	}
-	return (result *= sign % 2 == 0 ? -1 : 1);
+                                result = result * 10 - (s[index++] - 48);
+                                break;
+                }
+        }
+        return (result *= sign % 2 == 0 ? -1 : 1);
 }
