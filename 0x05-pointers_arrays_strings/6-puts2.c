ef22d13862a4325cb94041f7adae8386e3441542
kbwalex <kbwalex@gmail.com>
2022-12-19 16:04:00 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,14 +5,14 @@
  *
  * @str: arg *str
  *
- * Return: string
+ * Return: string to stdout
  */
 
 void puts2(char *str)
 {
 	int i;
 
-	while (str[i] != '\n')
+	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
 			_putchar(str[i]);
