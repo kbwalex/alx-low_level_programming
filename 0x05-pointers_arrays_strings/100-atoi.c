baf7154631cfd22b781547319b5d7063c049ca87
kbwalex <kbwalex@gmail.com>
2022-12-20 00:22:11 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,5 +9,25 @@
 
 int _atoi(char *s)
 {
-	return (*s);
+	int sign;
+	unsigned int num;
+	char *temp;
+
+	temt = s;
+	num = 0;
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+			sign *= -1;
+		temp++;
+	}
+	if (*temp != '\0')
+	{
+		do {
+			num = num * 10 + (*temp - '0');
+			temp++;
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (num * sign);
 }
