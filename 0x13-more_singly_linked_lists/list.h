785ef25dbce2ac8e7663311c3d7e20af67af9f89
kbwalex <kbwalex@gmail.com>
2023-01-31 15:09:46 -0800

    list.h

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -1,20 +1,29 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdio.h>
+
+/*
+ * File: lists.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x12-more_singly_linked_lists directory.
+ */
+
 #include <stdlib.h>
+
 /**
-* struct listint_s - singly linked list
-* @n: integer
-* @next: points to the next node
-*
-* Description: singly linked list node structure
-* for alx project
-*/
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -30,4 +39,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-#endif
+
+#endif /* LISTS_H */
